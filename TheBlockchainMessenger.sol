6a3fc73cf5dfd8de8ee3b9dfab654023d053e3ed
Lavric Cosmin-Marian <cosmin.lavric@student.unitbv.ro>
2024-04-10 13:24:00 +0300

    First Commit

---
+++ TheBlockchainMessenger.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+contract TheBlockchainMessenger{
+    uint public changeCounter;
+    address public owner;
+    string public theMessage;
+    constructor(){
+        owner=msg.sender;
+    }
+    function updateTheMessage(string memory _newMessage) public{
+        if(msg.sender==owner){
+            theMessage=_newMessage;
+            changeCounter++;
+        }
+    }
+}
